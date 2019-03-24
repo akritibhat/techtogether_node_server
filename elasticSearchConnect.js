cfa8d933f844e6d0c98e94c4cc42ad276bbd6cbb
rashichauhan7 <rashi.chauhan95@gmail.com>
2019-03-24 03:04:15 -0400

    added search

--- elasticSearchConnect.js
+++ elasticSearchConnect.js
@@ -5,11 +5,15 @@ module.exports = function (app) {
     app.get("/child", searchChild);
     app.get("/parent", searchParent)
 };
-    const request = require('request')
-
+    const request = require('request');
+    let parent1 = "";
+    let parent2 = "";
     function addChild(req, res) {
+        child = req.body;
+        parent1 = child.parent1;
+        parent2 = child.parent2;
         request.post('http://40.71.97.231:9200/children/profile', {
-            json: req
+            json: child
         }, (error, res, body) => {
             if (error) {
                 console.error(error)
@@ -47,10 +51,32 @@ module.exports = function (app) {
     }
 
     function searchChild(req, res) {
-
+        request.get('http://40.71.97.231:9200/_search?q=firstname:xyz||firstname=Akriti'
+        , (error, re, body) => {
+            if (error) {
+                console.error(error)
+                return
+            }
+            body = JSON.parse(body)
+            res.send(body)
+    });
     }
 
     function searchParent(req, res) {
-
+        request.get('http://40.71.97.231:9200/parent/_search', {
+            "query": {
+                "query_string" : {
+                    "default_field" : "content",
+                    "query" : req.params['query']
+                }
+            }
+        }, (error, re, body) => {
+            if (error) {
+                console.error(error)
+                return
+            }
+            body = JSON.parse(body)
+            res.send(body)
+        });
     }
 
